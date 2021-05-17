51afa433551627215416caefa9bf306593e03996
dave-howard <fatdaveh@gmail.com>
2021-05-17 19:51:45 +0100

    update version in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, Dave Howard'
 author = 'Dave Howard'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.3'
+release = '0.3.4'
 
 
 # -- General configuration ---------------------------------------------------
