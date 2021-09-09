301fbd53b126b900db5d4f8c22a3a16c90ab3652
Dave-Howard <fatdaveh@gmail.com>
2021-09-09 07:32:01 +0100

    v0.4.9 release

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, Dave Howard'
 author = 'Dave Howard'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.8'
+release = '0.4.9'
 
 
 # -- General configuration ---------------------------------------------------
