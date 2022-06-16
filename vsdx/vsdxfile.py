9b9ada74a22499fd5113bb3b7e6292c392963ba2
Ttislevoll <92029007+Ttislevoll@users.noreply.github.com>
2022-06-16 12:38:22 +0200

    Workaround when 'NameU' is not defined
    If for some reason some master page object doesn't have the attribute 'NameU' defined, it will be set to "unknown" to avoid a crash

--- vsdx/vsdxfile.py
+++ vsdx/vsdxfile.py
@@ -193,7 +193,12 @@ class VisioFile:
 
         # for each master page, create the Page object
         for master in self.masters_xml:
-            master_name = master.attrib['NameU']
+            try:
+				master_name = master.attrib['NameU']
+			except KeyError as e:
+				print(f'NameU not defined for object: {master.attrib["ID"]}')
+				master.attrib['NameU'] = "Unknown"
+                master_name = master.attrib['NameU']
             rel_id = master.find(f"{namespace}Rel").attrib[f"{r_namespace}id"]
             master_id = master.attrib['ID']
 
